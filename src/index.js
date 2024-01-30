ec1611f2960da2c90e1be3cb99609391510bf5cc
CoderRedwing <ajiteshru@gmail.com>
2024-01-30 20:27:43 +0530

    add model file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
-import  Express  from "express";
+import  express  from "express";
 import connectDB from "./db/index.js";
 import e from "express";
 
